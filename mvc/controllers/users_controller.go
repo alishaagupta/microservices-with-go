5e5b0ba7e349760b4a3abb2eafa235af527c429d
root <alishagupta262@gmail.com>
2020-01-07 16:07:47 +0530

    basic boilerplate

--- mvc/controllers/users_controller.go
+++ mvc/controllers/users_controller.go
@@ -1,21 +1,16 @@
-package controllers 
+package controllers
 
-import(
-	"net/http"
+import (
 	"log"
+	"net/http"
 	"strconv"
+
 	"github.com/alishaagupta/microservices-with-go/mvc/services"
 	"github.com/alishaagupta/microservices-with-go/mvc/utils"
-
-	"encoding/json"
 	"github.com/gin-gonic/gin"
-
-
-
 )
 
-func GetUser(c *gin.Context){
-
+func GetUser(c *gin.Context) {
 
 	// userIdParam := req.URL.Query().Get("user_id");
 
@@ -24,22 +19,23 @@ func GetUser(c *gin.Context){
 
 	log.Printf("About to process user_id %v ", userIdParam)
 
-	userId, err := strconv.ParseInt(userIdParam, 10, 64) 
+	userId, err := strconv.ParseInt(userIdParam, 10, 64)
 
 	if err != nil {
 
 		apiErr := &utils.ApplicationError{
-			Message: "user_id must be a number ",
+			Message:    "user_id must be a number ",
 			StatusCode: http.StatusBadRequest,
-			Code: "bad_request",
+			Code:       "bad_request",
 		}
 
 		// Equivalent to writing reposne to the client
 
-		c.JSON(apiErr.StatusCode, apiErr)
+		utils.Respond(c, apiErr.StatusCode, apiErr)
+		// c.JSON(apiErr.StatusCode, apiErr)
 
 		// jsonValue, _ := json.Marshal(apiErr)
-		// // Return the error to the client 
+		// // Return the error to the client
 		// resp.WriteHeader(apiErr.StatusCode)
 		// resp.Write(jsonValue)
 		return
@@ -47,10 +43,12 @@ func GetUser(c *gin.Context){
 
 	user, apiErr := services.GetUser(userId)
 
-	if apiErr!= nil{
+	if apiErr != nil {
 		// Handle the error
 
-		c.JSON(apiErr.StatusCode, apiErr)
+		utils.Respond(c, apiErr.StatusCode, apiErr)
+
+		// c.JSON(apiErr.StatusCode, apiErr)
 		// resp.WriteHeader(apiErr.StatusCode)
 		// resp.Write([]byte(apiErr.Message))
 		return
@@ -58,7 +56,9 @@ func GetUser(c *gin.Context){
 
 	// Return user to the client
 
-	c.JSON(http.StatusOK, user)
+	utils.Respond(c, http.StatusOK, user)
+
+	// c.JSON(http.StatusOK, user)
 	// jsonValue, _ := json.Marshal(user)
 	// resp.Write(jsonValue)
 }
